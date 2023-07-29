aeef5ed35624bd5c4d16f25bd1198325e15161b1
James <jameskr97@gmail.com>
2023-07-29 16:43:58 -0400

    Minimum necessary changes to be detected by IIDX Infinitas

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -51,8 +51,8 @@ tusb_desc_device_t const desc_device_joy = {
     .bDeviceProtocol = 0x00,
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor = 0xCafe,
-    .idProduct = USB_PID,
+    .idVendor = 0x1ccf,
+    .idProduct = 0x8048,
     .bcdDevice = 0x0100,
 
     .iManufacturer = 0x01,
@@ -70,8 +70,8 @@ tusb_desc_device_t const desc_device_key = {
     .bDeviceProtocol = 0x00,
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor = 0xCafe,
-    .idProduct = USB_PID,
+    .idVendor = 0x1ccf,
+    .idProduct = 0x8048,
     .bcdDevice = 0x0100,
 
     .iManufacturer = 0x01,
@@ -159,8 +159,8 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const* string_desc_arr[] = {
     (const char[]){0x09, 0x04},  // 0: is supported language is English (0x0409)
-    "SpeedyPotato",              // 1: Manufacturer
-    "Pico Game Controller",      // 2: Product
+    "Konami Amusement",              // 1: Manufacturer
+    "beatmania IIDX controller premium model",      // 2: Product
     "123456",                    // 3: Serials, should use chip ID
     "Button 1",
     "Button 2",
